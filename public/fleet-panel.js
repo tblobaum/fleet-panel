ccf05ba1a50181fd968ace3486f66b2d8c8ff1f6
Thomas Blobaum <tblobaum@gmail.com>
2012-08-04 04:25:45 -0500

    remove some unneccessary properties

--- public/fleet-panel.js
+++ public/fleet-panel.js
@@ -11,18 +11,13 @@ module.exports = function (opts) {
 function Panel (opts) {
   var self = this
   opts = opts || {}
-
   if (!opts.secret) {
     opts.secret = prompt('Enter the hub password', 'beepboop')
   }
-
   self.secret = opts.secret || ''
-  self.host = opts.host || 'localhost'
-  self.port = opts.port || 80
   var stream = shoe('/dnode')
   var d = dnode()
   d.on('remote', function (remote) {
-    var auth = self.secret && 'git:' + encodeURIComponent(self.secret)
     remote.auth(self.secret, function (err, result) {
       if (err) self.emit('error', err)
       else {
