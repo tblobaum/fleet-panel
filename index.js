908c9a7db93358ba5ac73da3281949e9e65d64aa
Thomas Blobaum <tblobaum@gmail.com>
2012-08-09 07:36:18 -0500

    use figc to process command line arguments

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-
-var http = require('http')
+var figc = require('figc')
+  , config = figc(process.cwd() + '/fleet-panel.json')
+  , http = require('http')
   , shoe = require('shoe')
   , ecstatic = require('ecstatic')(__dirname + '/public')
   , dnode = require('dnode')
   , server = http.createServer(ecstatic)
 
-server.listen(process.argv[2] || 3000)
+server.listen(config.port || 3000)
 
 // if you are running fleet-panel from the 
 // same origin as a fleet-hub then this is not needed
-
 var sock = shoe(function (stream) {
-  dnode.connect(process.argv[4] || 9000, process.argv[3] || 'localhost', function (remote, conn) {
+  dnode.connect({
+      port : config.hub.split(':')[1]
+    , host : config.hub.split(':')[0]
+  }, function (remote, conn) {
     var d = dnode(remote)
     d.pipe(stream).pipe(d)
   })
